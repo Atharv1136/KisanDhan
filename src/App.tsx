e56edd1c24afd1dc3b105676e047224203ad68f3
Atharv Bhosale <120968231+Atharv1136@users.noreply.github.com>
2025-05-14 17:04:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -54,19 +54,4 @@ function App() {
             <Route path="/shopkeeper/add-shop" element={<AddShop />} />
             <Route path="/shopkeeper/manage-crops" element={<ManageCrops />} />
             
-            {/* Common Routes */}
-            <Route path="/account" element={<UserAccount />} />
-            <Route path="/admin" element={<AdminDashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/404" element={<NotFound />} />
-            <Route path="*" element={<Navigate to="/404" replace />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
-
-export default App;
+           
